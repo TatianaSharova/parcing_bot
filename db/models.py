d5e05a7bb154b7ae58838054a36a8b01a0750a52
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-03-22 14:00:55 +0400

    add first ver Data model

---
+++ db/models.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from sqlalchemy import Float, Integer, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Data(Base):
+    '''
+    Модель для хранения информации о сайтах по продаже зюбликов.
+    '''
+    __tablename__ = 'data'
+
+    id: Mapped[int] = mapped_column(primary_key=True,
+                                    autoincrement=True)
+    title: Mapped[str] = mapped_column(String(20))
+    url: Mapped[str] = mapped_column(String(50))
+    xpath: Mapped[str] = mapped_column(String(50))
